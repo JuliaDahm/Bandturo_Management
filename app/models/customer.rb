13491212116e2ee60f98c5f0c615492acd051c5b
Julia Dahm <jukabrown@gmail.com>
2016-12-05 20:28:16 -0500

    add aws urls for static images

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,18 +1,18 @@
 class Customer < ActiveRecord::Base
 
-  has_attached_file :main_photo, default_url: "missing-image.png"
+  has_attached_file :main_photo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
   validates_attachment_content_type :main_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :live_photo, default_url: "missing-image.png"
+  has_attached_file :live_photo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :logo, default_url: "missing-logo.png"
+  has_attached_file :logo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-logo.png"
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :misc_photo1, default_url: "missing-image.png"
+  has_attached_file :misc_photo1, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :misc_photo2, default_url: "missing-image.png"
+  has_attached_file :misc_photo2, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
   scope :parameterized_search, 
