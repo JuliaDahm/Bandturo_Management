18a02ab8a5c12fc40e971930e222f3f6e47ca49f
Julia Dahm <jukabrown@gmail.com>
2017-01-03 17:58:33 -0500

    Create AWS user with full access, use new credentials

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,7 @@ class Customer < ActiveRecord::Base
 	                  :s3_permissions => {:original => "private"}
 
 	def s3_credentials
-	  {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :s3_region => ENV["AWS_REGION"]}
+	  {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :region => ENV["AWS_REGION"]}
 	end
 
   has_attached_file :main_photo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
