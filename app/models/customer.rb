676ae62033dc99bce04bf06a414ef44e468c1d07
Julia Dahm <jukabrown@gmail.com>
2017-01-04 17:57:52 -0500

    Move S3 credentials to Paperclip defaults hash; Organize Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,26 +1,45 @@
 class Customer < ActiveRecord::Base
-	has_attached_file :download,
-	                  :storage => :s3,
-	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
-	                  :s3_permissions => {:original => "private"}
-
 	def s3_credentials
-	  {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID"], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :region => ENV["AWS_REGION"]}
+    {
+     	:bucket => ENV['S3_BUCKET_NAME'],
+     	:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+     	:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+     	:region => ENV['AWS_REGION']
+     }
 	end
 
-  has_attached_file :main_photo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
+	has_attached_file :download,
+	                  :storage => :s3,
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  has_attached_file :main_photo, 
+  									:storage => :s3,
+  									:default_url => "https://s3.amazonaws.com/bandturomanagement/missing-image.png", 
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :main_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :live_photo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
+  has_attached_file :live_photo, 
+  									:storage => :s3,
+  									:default_url => "https://s3.amazonaws.com/bandturomanagement/missing-image.png", 
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :logo, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-logo.png"
+  has_attached_file :logo, 
+  									:storage => :s3,
+  									:default_url => "https://s3.amazonaws.com/bandturomanagement/missing-logo.png", 
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :misc_photo1, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
+  has_attached_file :misc_photo1, 
+  									:storage => :s3,
+  									:default_url => "https://s3.amazonaws.com/bandturomanagement/missing-image.png", 
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :misc_photo2, default_url: "https://s3.amazonaws.com/bandturomanagement/missing-image.png"
+  has_attached_file :misc_photo2, 
+  									:storage => :s3,
+  									:default_url => "https://s3.amazonaws.com/bandturomanagement/missing-image.png", 
+	                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
   validates_attachment_content_type :live_photo, content_type: /\Aimage\/.*\z/
 
   scope :parameterized_search, 
