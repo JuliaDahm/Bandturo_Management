d973c2579ca725e9b59e2cb3d0152f131da71013
Julia Dahm <jukabrown@gmail.com>
2017-04-26 17:56:26 -0400

    corrected action mailer default url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,9 @@ Rails.application.configure do
   config.serve_static_assets = false
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'  
   
-  config.action_mailer.default_url_options = { :host => 'bandturomanagement.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'bandturomanagement.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default :charset => "utf-8" 
   config.action_mailer.smtp_settings = {
